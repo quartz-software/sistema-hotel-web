a0d764a42bcc558ba6a994865e1b40ff1a338d0a
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2024-09-30 06:35:44 -0400

    modelo User creado

---
+++ server/models/User.js
@@ -0,0 +1,43 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(500),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "users",
+    timestamps: false,
+  }
+);
+
+export default User;
