5194189553fe8d464e04c8062ebc1cee596899ba
papenfab3 <faberfabrimonterogonzales@gmail.com>
2024-10-10 01:29:32 -0400

    modelo detalle pago

---
+++ server/models/DetallePago.js
@@ -0,0 +1,42 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import Pago from "./Pago.js"; 
+
+const DetallePago = sequelize.define(
+  "DetallePago",
+  {
+    id: {
+      type: DataTypes.BIGINT,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    concepto: {
+      type: DataTypes.STRING(100), 
+      allowNull: false,
+    },
+    monto: {
+      type: DataTypes.DECIMAL(10, 2), 
+      allowNull: false,
+    },
+    pagado: {
+      type: DataTypes.BOOLEAN, 
+      allowNull: false,
+      defaultValue: false, 
+    },
+    idPago: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: Pago,
+        key: "id",
+      },
+    },
+  },
+  {
+    tableName: "detallePagos", 
+    timestamps: false, 
+  }
+);
+DetallePago.belongsTo(Pago, { foreignKey: "idPago" });
+
+export default DetallePago;
