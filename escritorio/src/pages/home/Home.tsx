d2571e8e8fccc4dfe921b7c48c074840d7d191c7
ASO <Alvarito1624@gmail.com>
2024-10-10 20:40:46 -0400

    creacion input/formFiled

--- escritorio/src/pages/home/Home.tsx
+++ escritorio/src/pages/home/Home.tsx
@@ -1,11 +1,15 @@
 import viteLogo from "/vite.svg";
 import reactLogo from "./../common/assets/react.svg";
+import FormField from "../common/FormField/FormField";
 const Home = () => {
   return (
     <>
       <div>Home</div>
       <img src={viteLogo} alt="" />
       <img src={reactLogo} alt="" />
+      <FormField
+        title="Nombre"
+      />
     </>
   );
 };
