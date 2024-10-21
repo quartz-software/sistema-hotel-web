c1794069e7eb894ca7692bcf47a812e705a51160
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2024-10-21 18:20:45 -0400

    dependencia cookie-parser agregada

--- server/app.js
+++ server/app.js
@@ -4,13 +4,16 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { fileURLToPath } from "url";
 import router from "./router.js";
+import cookieParser from "cookie-parser";
+import env from "dotenv";
+env.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = Express();
 
-app.use(bodyParser.json());
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(Express.static(path.join(__dirname, "dist")));
