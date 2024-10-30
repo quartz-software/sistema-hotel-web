7ff1ad550ccc53a4dd5a3fd25552f1d9c4c796e9
ASO <Alvarito1624@gmail.com>
2024-10-30 08:26:00 -0400

    formulario de habitaciones

---
+++ escritorio/src/pages/habitacion/Habitaciones.tsx
@@ -0,0 +1,11 @@
+import Habitaciones_formulario from "./Habitaciones_formulario";
+
+const Habitaciones=()=>{
+    return(
+       <Habitaciones_formulario/>
+
+    );
+    
+}
+
+export default Habitaciones
