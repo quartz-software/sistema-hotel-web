865f41a1b88ea829db9d70a1700063ac152fa711
SerVi8 <sergiovargasumacacho@gmail.com>
2024-11-06 03:57:21 -0400

    modelo stock creado

--- server/models/Stock.js
+++ server/models/Stock.js
@@ -12,6 +12,42 @@ const Stock = sequelize.define(
         isInt: true,
       },
     },
+    name: {
+      type: DataTypes.STRING(75),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    cantida: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+        min: 0,
+      }
+    },
+    unidM: {
+      type: DataTypes.STRING(5),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    category: {
+      type: DataTypes.STRING(50),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    fachaAbastecimiento: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      }
+    }
   },
   {
     tableName: "stock",
