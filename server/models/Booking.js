8c861c2e3e392246f0739e10d3c3fa401e58b07e
SerVi8 <sergiovargasumacacho@gmail.com>
2024-11-06 03:38:50 -0400

    modelo booking

--- server/models/Booking.js
+++ server/models/Booking.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../config/db.js";
+import Client from "./Client.js";
 
 const Booking = sequelize.define(
   "Booking",
@@ -12,6 +13,74 @@ const Booking = sequelize.define(
         isInt: true,
       },
     },
+    numAdultos: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+        min: 1
+      }
+    },
+    numNinos: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        isInt: true,
+        min: 0,
+      }
+    },
+    fechaReceva: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    checkIn: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    checkOut: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      }
+    },
+    status: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isIn: [['En Curso', 'Cancelada', 'Teminada', 'Pendiente']]
+      }
+    },
+    totalPrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+        min: 0.0,
+      }
+    },
+    bookingOrigin: {
+      type: DataTypes.STRING(60),
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    idEmployee: {
+    },
+    idCliet: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: Client,
+        key: 'id'
+      }
+    }
   },
   {
     tableName: "bookings",
