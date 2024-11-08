bf8ffd248ebc746c3bbba67aed00a9ed342a4349
SerVi8 <sergiovargasumacacho@gmail.com>
2024-11-08 01:32:46 -0400

    modelo task creado

--- server/models/Task.js
+++ server/models/Task.js
@@ -12,6 +12,55 @@ const Task = sequelize.define(
         isInt: true,
       },
     },
+    status: {
+      type: DataTypes.STRING(15),
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+        isIn: [['pendiente', 'en progreso', 'completada']],
+      }
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    dateCreate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW
+    },
+    dateStart: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    dateFinished: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    employeeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "employees",
+        key: "id",
+      },
+    },
+    employeeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "employees",
+        key: "id",
+      },
+    },
+    roomId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "rooms",
+        key: "id",
+      },
+    },
   },
   {
     tableName: "tasks",
