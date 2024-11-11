284ee307ef1f15ecc165ac9cd75c25f29d1ee81f
Joel Alejandro Sanchez Valdez <joelalejsanchez@gmail.com>
2024-11-11 06:22:29 -0400

    correccion a ClientController

--- server/controllers/ClientController.js
+++ server/controllers/ClientController.js
@@ -9,8 +9,7 @@ export default class ClientController {
   static async findAll(req, res) {
     try {
       const clients = await Client.findAll();
-      console.log(clients);
-      res.status(200).send();
+      res.status(200).json(clients);
     } catch (e) {
       res.status(500).send();
     }
@@ -25,10 +24,10 @@ export default class ClientController {
       const id = req.params.id;
       if (!id) return res.status(400).send();
 
-      const room = await Client.findByPk(id);
-      if (!room) return res.status(404).send();
+      const client = await Client.findByPk(id);
+      if (!client) return res.status(404).send();
 
-      res.status(200).json(room);
+      res.status(200).json(client);
     } catch (e) {
       res.status(500).send();
     }
@@ -41,8 +40,8 @@ export default class ClientController {
   static async create(req, res) {
     try {
       const body = req.body;
-      const rooms = await Client.create(body);
-      res.status(201).json(rooms);
+      const client = await Client.create(body);
+      res.status(201).json(client);
     } catch (e) {
       res.status(500).send();
     }
